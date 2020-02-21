3b84405f352c70464ad18d7fac5b62dd8ff08f73
Jason Fulks <jason.fulks.2023@student.lhschools.org>
2020-02-21 12:22:02 -0500

    Oddness Method Created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        testOddness();
+    }
+
+
+        public static void testOddness(){
+	 Scanner keyboard;
+	 keyboard = new Scanner (System.in);
+	 int number;
+            System.out.println("Give me a positive number.");
+            number = keyboard.nextInt();
+            System.out.println("Number is even " + (number % 2==0));
+            System.out.println("Number is odd " + (number % 2!=0));
     }
 }
