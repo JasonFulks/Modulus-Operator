47147bdb2b413cff02586260fd8d478136c105cc
Jason Fulks <jason.fulks.2023@student.lhschools.org>
2020-03-05 11:16:01 -0500

    lastDigitMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) {
         testOddness();
         keepContained();
+        lastDigit();
+
 
     }
 
@@ -30,9 +32,19 @@ public class Main {
         int number;
         System.out.println("Give me a positive number.");
         number = keyboard.nextInt();
-System.out.println("Number contained is "+ ((number%8)+5));
+        System.out.println("Number contained is " + ((number % 8) + 5));
 
     }
 
+    public static void lastDigit() {
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int number;
+        System.out.println("Give me a four digit, positive number.");
+        number = keyboard.nextInt();
+        System.out.println("The last digit is " + (number%10));
+
+
+    }
 }
 
